6827946856c896337df8a04566a3eaef1aafaddb
Ellie Keen <elliekeen95@gmail.com>
2018-05-27 19:37:42 +0100

    Refactor repetition in tests

--- spec/feature/RunGameSpec.js
+++ spec/feature/RunGameSpec.js
@@ -26,21 +26,16 @@ describe('complete frame', function(){
 
 describe('bowling three balls', function(){
   it('should not increase frame when the frame is not complete', function(){
-    game.bowl(3);
-    game.bowl(4);
-    game.bowl(2);
+    for (var i = 0; i < 3; i++) { game.bowl(3); }; 
     expect(game.frame).toEqual(1);
   });
 });
 
 describe('two complete frames', function(){
   it('should increase frame to 2, increment score and reset remaining pins', function(){
-    game.bowl(3);
-    game.bowl(4);
-    game.bowl(2);
-    game.bowl(5);
+    for (var i = 0; i < 4; i++) { game.bowl(3); };
     expect(game.frame).toEqual(2);
-    expect(game.score).toEqual(14);
+    expect(game.score).toEqual(12);
     expect(game.remainingPins).toEqual(10);
   });
 });
@@ -48,29 +43,11 @@ describe('two complete frames', function(){
 describe('complete game', function(){
 
   beforeEach(function(){
-    game.bowl(3);
-    game.bowl(4);//1st frame
-    game.bowl(2);
-    game.bowl(5);//2nd frame
-    game.bowl(3);
-    game.bowl(4);//3rd
-    game.bowl(2);
-    game.bowl(5);//4th
-    game.bowl(3);
-    game.bowl(4);//5th
-    game.bowl(2);
-    game.bowl(5);//6th
-    game.bowl(3);
-    game.bowl(4);//7th
-    game.bowl(2);
-    game.bowl(5);//8th
-    game.bowl(3);
-    game.bowl(4);//9th
-    game.bowl(2);
+    for (var i = 0; i < 19; i++) { game.bowl(3); };
   });
 
   it('should state game over and show score', function(){
-    expect(game.bowl(5)).toEqual('Game over. You scored 70!');
+    expect(game.bowl(3)).toEqual('Game over. You scored 60!');
   });
 
   it('should reset the game', function(){
@@ -80,30 +57,11 @@ describe('complete game', function(){
     expect(game.frame).toEqual(0);
     expect(game.firstBowlOfFrame).toBeTruthy();
   });
-
 });
 
 describe('gutter game', function(){
   it('should state that the player has scored 0', function(){
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
-    game.bowl(0);
+    for (var i = 0; i < 19; i++) { game.bowl(0); };
     expect(game.bowl(0)).toEqual('Game over. You scored 0!');
   });
 });
