1b73b594f5fa1e86e42b8867e9bf5e9845464627
Ellie Keen <elliekeen95@gmail.com>
2018-05-27 16:44:41 +0100

    Introduced frames which require two bowls

--- spec/feature/RunGameSpec.js
+++ spec/feature/RunGameSpec.js
@@ -2,11 +2,33 @@ beforeEach(function(){
   game = new Game();
 });
 
-describe('after bowling one ball on first frame', function(){
-  it('should increment score and change remaining pins', function(){
+describe('bowling one ball on first frame', function(){
+  it('should increment score and change remainingPins', function(){
     game.bowl(5);
     expect(game.remainingPins).toEqual(5);
     expect(game.score).toEqual(5);
   });
+});
 
+describe('complete frame', function(){
+  it('should change score and increase frame by one', function(){
+    game.bowl(2);
+    game.bowl(3);
+    expect(game.score).toEqual(5);
+    expect(game.frame).toEqual(1);
+  });
+
+  it('should raise error if user tries to knock down more than 10 pins', function(){
+    game.bowl(8);
+    expect(function() { game.bowl(8); }).toThrow('Bad bowl! There are 2 pins remaining!');
+  });
+});
+
+describe('bowling three balls', function(){
+  it('should not increase frame when the frame is not complete', function(){
+    game.bowl(3);
+    game.bowl(4);
+    game.bowl(2);
+    expect(game.frame).toEqual(1);
+  });
 });
--- src/Game.js
+++ src/Game.js
@@ -2,9 +2,31 @@ function Game() {
   this.frame = 0;
   this.score = 0;
   this.remainingPins = 10;
+  this.firstBowl = true;
 };
 
 Game.prototype.bowl = function (pins) {
-  this.remainingPins -= pins;
+  this._checkPins(pins);
+  if (this.firstBowl) {
+    this._firstBowl(pins);
+  } else {
+    this._secondBowl();
+  };
   this.score += pins;
+};
+
+Game.prototype._checkPins = function(pins) {
+  if (this.remainingPins - pins < 0) {
+    throw `Bad bowl! There are ${this.remainingPins} pins remaining!`;
+  };
+};
+
+Game.prototype._firstBowl = function (pins) {
+  this.remainingPins -= pins;
+  this.firstBowl = false;
+};
+
+Game.prototype._secondBowl = function () {
+  this.frame += 1;
+  this.firstBowl = true;
 };
